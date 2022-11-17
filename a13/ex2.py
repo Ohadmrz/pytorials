ba75c263b5c00abc7ed22db5f61da9cbdd75a38a
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2022-11-17 10:57:15 +0200

    lesson 4 - code

--- a13/ex2.py
+++ a13/ex2.py
@@ -8,3 +8,35 @@ for i in nums:
     elif min_num > i:
         min_num = i
 print(min_num)
+
+
+# Ziv' s solution
+# num_list = []
+# flag = True
+# temp = None
+#
+# while flag:
+#     num = input("Enter a number to the list, $ to stop: ")
+#     if num == '$':
+#         flag = False
+#         break
+#     else:
+#         if not num.isdigit():
+#             print("Invalid input. Use numbers.")
+#             continue
+#         else:
+#             num = int(num)
+#             num_list.append(num)
+#             continue
+#
+# print(num_list)
+#
+# for x in range(len(num_list)):
+#     if x == 0:
+#         temp = num_list[x]
+#         continue
+#     else:
+#         if num_list[x] < num_list[x-1]:
+#             temp = num_list
+#
+# print(temp)
---
+++ a15/ex4.py
@@ -0,0 +1,4 @@
+max_num = int(input("Enter a maximum positive number: "))
+for i in range(1, max_num + 1):
+    cubed_num = i ** 3
+    print(f"Current Number is : {i}  and the cube is {cubed_num}")
