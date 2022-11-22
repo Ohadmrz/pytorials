d69a3b47d8b6f93825e0810293e7a2646c20f9f5
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2022-11-22 21:03:43 +0200

    lesson 6 init

--- b1/ex8.py
+++ b1/ex8.py
@@ -9,4 +9,16 @@ def perfect(number:int ) -> bool :
         return True
     else:
         return False
+
+
+def perfect2(number: int) -> bool:
+    dividers_sum = 0
+    if number == 0:
+        return False
+    for i in range(1, number):
+        if number % i == 0:
+            dividers_sum += i
+    return dividers_sum == number
+
+
 print(perfect(496))
