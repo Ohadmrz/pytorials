63516fcfde810b319429379d62e24f64f11bf8f2
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2022-12-18 19:40:43 +0200

    lesson 2 eve

---
+++ a9/ex1.py
@@ -0,0 +1,2 @@
+word = input("Insert your word: ")
+print(f"There are {len(word)} chars in the word")
---
+++ a9/ex5_base.py
@@ -0,0 +1,12 @@
+b1 = input("Insert batch 1: ")
+b2 = input("Insert batch 2: ")
+b3 = input("Insert batch 3: ")
+
+print(len(b1), len(b2), end=" ", sep="-")
+if b3 != "":
+    print(len(b3))
+# if len(b3) == 0
+# if b3 == "":
+#     print(len(b1), len(b2))
+# else:
+#     print(len(b1), len(b2), len(b3))
