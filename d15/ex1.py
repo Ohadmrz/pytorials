545b2e321b2448f92af49f41e5ad43e9f3996223
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2022-12-21 13:44:42 +0200

    various

--- d15/ex1.py
+++ d15/ex1.py
@@ -5,9 +5,21 @@ def single_str_arg(func):
 
     def wrapper_func(*args, **kwargs):
 
-        if len(args) != 1 or len(kwargs) != 0 or not isinstance(args[0], str):
+        if len(args) != 1 or len(kwargs) != 0 or \
+                not isinstance(args[0], str):
             raise InvalidArgument()
         return func(*args, **kwargs)
 
     return wrapper_func
 
+# @single_str_arg
+# def convert_to_upper(word: str) -> str:
+#     return word.upper()
+#
+# convert_to_upper = single_str_arg(convert_to_upper)
+#
+#
+# if __name__ == '__main__':
+#     # convert_to_upper(5)
+#     convert_to_upper("hello")
+#     single_str_arg(convert_to_upper)("hello")
--- d15/tests.py
+++ d15/tests.py
@@ -1,19 +1,29 @@
 from d15.ex1 import single_str_arg
 from d15.ex3 import valid_param_types
 
+class Company:
+    pass
 
 @single_str_arg
 def convert_to_upper(word: str) -> str:
     return word.upper()
 
-@valid_param_types([int])
+@valid_param_types([int, float])
 def get_sum(num1, num2):
     return num1 + num2
+
+@valid_param_types([Company])
+def my_print(*args):
+    print(*args)
+
+# get_sum = valid_param_types([int, float])(get_sum)(4, 5)
 
 if __name__ == '__main__':
     # convert_to_upper(5)
     # convert_to_upper("hello")
-    get_sum(4,5)
+    # get_sum(4,5)
     # get_sum("a", "b")
+
+    my_print(Company())
 
     # valid_param_types([int])(get_sum)(3,4)
