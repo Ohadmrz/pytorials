bd3809a91dd0ddf811bc268579797d0efdf0d6a1
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2022-12-21 21:44:12 +0200

    various

--- a10/ex8.py
+++ a10/ex8.py
@@ -1,6 +1,7 @@
 # Write a program that counts the total number of digits
 # in a number received from a user using a while loop.
 
+
 num = int(input("Insert num: "))
 digits_num = 0
 while num > 0:
