b83117f2c4be07947aa78ea5f8d4b95d14b9ecb5
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2023-02-06 20:38:26 +0200

    jb - less 23

--- d3/ex2.py
+++ d3/ex2.py
@@ -1,11 +1,18 @@
 import math
 
 
-def batches(n, my_list):
-    start_idx = 0
-    for i in range(math.ceil(len(my_list)/n)):
-        yield my_list[start_idx:start_idx+n]
-        start_idx =  start_idx + n
+# def batches(n, my_list):
+#     start_idx = 0
+#     for i in range(math.ceil(len(my_list)/n)):
+#         yield my_list[start_idx:start_idx+n]
+#         start_idx =  start_idx + n
+#
+
+def batches(n: int, l: list):
+    i = 0
+    while i < len(l):
+        yield l[i: i+n]
+        i += n
 
 
 if __name__ == '__main__':
