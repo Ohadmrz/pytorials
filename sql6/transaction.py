bc15a6a0c0a5be7c1d62d5ebe14d65810f68af04
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2023-03-09 21:53:58 +0200

    sql6 transaction solution

---
+++ sql6/transaction.py
@@ -0,0 +1,46 @@
+import datetime
+
+import psycopg2
+
+
+def transfer(from_account, to_account, amount, initiated_by):
+    insert_trans_query = f"""
+             INSERT INTO public.transactions
+             (transaction_type, ts, initiated_by)
+             VALUES(%s, %s, %s) returning id;
+             """
+    insert_trans_account_query = f"""
+                             INSERT INTO public.transaction_accounts
+                             (account_role, transaction_id, account_id)
+                             VALUES( %s, %s, %s );
+                             """
+    reduce_balance_query = f"""
+                             UPDATE public.accounts
+                             SET balance= balance - %s
+                             WHERE id=%s
+                             """
+    increase_balance_query = f"""
+                             UPDATE public.accounts
+                             SET balance= balance + %s
+                             WHERE id=%s
+                             """
+
+    conn = psycopg2.connect(
+        host='localhost',
+        port=5432,
+        database='bank',
+        user='postgres',
+        password='postgres')
+
+    with conn:
+        with conn.cursor() as cur:
+            cur.execute(insert_trans_query, ('transfer', datetime.datetime.now(), initiated_by))
+            transfer_id = cur.fetchone()[0]
+            cur.execute(insert_trans_account_query, ('receiver', transfer_id, to_account ))
+            cur.execute(insert_trans_account_query, ('sender', transfer_id, from_account))
+            cur.execute(increase_balance_query, (amount, to_account))
+            cur.execute(reduce_balance_query, (amount, from_account))
+
+
+if __name__ == '__main__':
+    transfer(1, 2, 1000, 1)
